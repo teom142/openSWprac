8794257de57a26a95d43f95e94489f5cc05d3fbd
teom142 <xodn9245@naver.com>
2024-03-08 15:09:55 +0900

    엄태우 202012038

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-name, sch_num = input("이름과 학번을 입력하세요(예시 엄태우 2020112038)").split()
+name, sch_num, major = input("이름과 학번 및 학과를 입력하세요(예시 엄태우 2020112038 컴퓨터공학과)").split()
 
-print("이름: {}\n학번: {}".format(name, sch_num))
+print("이름: {}\n학번: {}\n학과: {}".format(name, sch_num, major))
